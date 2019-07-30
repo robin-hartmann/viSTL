b856744aa42a8e57b4a00ba4ee60aad01d2127ed
Alexandr Timchenko <atimchenko92@gmail.com>
2019-07-30 17:33:10 +0200

    Added invertion option. (Needs to be fixed)

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import argparse
 import re
+import numpy
 
 from stl_tools import numpy2stl, text2png
 from scipy.ndimage import gaussian_filter
 from pylab import imread
+from PIL import Image
+import PIL.ImageOps
 
 MAX_HEIGHT = 200
 MAX_WIDTH = 200
-MAX_DEPTH = 50
+MAX_DEPTH = 40
 OUTPUT_DIR = "stl_out/"
 
 
@@ -35,23 +38,26 @@ def test_txt2stl_braille():
               solid=True)
 
 
-def process_input(file):
+def process_input(file, args):
     if file.name.lower().endswith('.png'):
-        process_image(file)
+        process_image(file, args)
     else:
-        process_text(file)
+        process_text(file, args)
 
 
-def process_text(file):  # TODO
+def process_text(file, args):  # TODO
     print "Processing text: %s" % file.name
 
 
-def process_image(file):
-    # TODO: invertion flag?
+def process_image(file, args):
     print "Processing image: %s" % file.name
     fname_pure = re.search('^(.+)/([^/]+).png', file.name)
+    if args.i is True:
+        print "Inverting image..."
+        A = 256 * invert_image(file.name)
+    else:
+        A = 256 * imread(file.name)
 
-    A = 256 * imread(file.name)
     A = A[:, :, 2] + 0.5*A[:, :, 0]
     A = gaussian_filter(A, 2)  # smoothing
     numpy2stl(A,
@@ -61,14 +67,18 @@ def process_image(file):
               solid=True)
 
 
-def validate_inputs(inputs):
+def invert_image(file):
+    return numpy.asarray(PIL.ImageOps.invert(Image.open(file)))
+
+
+def validate_inputs(args):
     check = lambda fname: fname.lower().endswith(('.png', '.txt'))
-    for input_file in inputs:
+    for input_file in args.inputs:
         if not check(input_file.name):
             sys.stderr.write('All inputs must be .png or .txt')
             sys.exit(1)
         else:
-            process_input(input_file)
+            process_input(input_file, args)
 
 
 if __name__ == "__main__":
@@ -77,6 +87,9 @@ if __name__ == "__main__":
     parser.add_argument('-t',
                         action='store_true',
                         help='runs all tests')
+    parser.add_argument('-i',
+                        action='store_true',
+                        help='inverts the picture colors')
     parser.add_argument('inputs',
                         nargs='*',
                         type=argparse.FileType('r'),
@@ -88,4 +101,4 @@ if __name__ == "__main__":
         test_img2stl_helloworld()
         test_txt2stl_braille()
     else:
-        validate_inputs(args.inputs)
+        validate_inputs(args)
