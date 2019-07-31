2ece7b89acf571197f344a7c19a0a7d6a84cb4b7
Alexandr Timchenko <atimchenko92@gmail.com>
2019-07-31 15:44:45 +0200

    Code refactoring: argument parsing. Added input dir reading

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import argparse
 import re
 import numpy
@@ -12,9 +14,10 @@ MAX_HEIGHT = 200
 MAX_WIDTH = 200
 MAX_DEPTH = 40
 OUTPUT_DIR = "stl_out/"
+DEFAULT_INPUT_DIR = "inputs/"
 
 
-def test_img2stl_helloworld():
+def test_img2stl():
     A = 256 * imread("examples/helloWorld.png")
     A = A[:, :, 2] + 1.0*A[:, :, 0]  # Compose RGBA channels to give depth
     A = gaussian_filter(A, 1)  # smoothing
@@ -27,32 +30,31 @@ def test_img2stl_helloworld():
               solid=True)
 
 
-def test_txt2stl_braille():
-    A = 256 * imread("examples/braille_inverse.png")
-    A = A[:, :, 2] + 0.5*A[:, :, 0]  # Compose RGBA channels to give depth
-    A = gaussian_filter(A, 2)  # smoothing
-    numpy2stl(A,
-              "examples/braille_inverse.stl",
-              scale=0.05,
-              mask_val=5.,
-              solid=True)
+def test_txt2braille():
+    print "TODO"
+
+
+def test_braille2stl():
+    print "TODO"
 
 
 def process_input(file, args):
     if file.name.lower().endswith('.png'):
         process_image(file, args)
-    else:
+    elif file.name.lower().endswith('.txt'):
         process_text(file, args)
+    else:
+        print "All inputs must be .png or .txt. Skip"
 
 
-def process_text(file, args):  # TODO
-    print "Processing text: %s" % file.name
+def process_text(file, args):
+    print "TODO: Processing text: %s" % file.name
 
 
 def process_image(file, args):
     print "Processing image: %s" % file.name
     fname_pure = re.search('^(.+)/([^/]+).png', file.name)
-    if args.i is True:
+    if args.inv is True:
         A = invert_image(file.name)
     else:
         A = 256 * imread(file.name)
@@ -70,14 +72,23 @@ def invert_image(file):
     return numpy.asarray(PIL.ImageOps.invert(Image.open(file)))
 
 
-def validate_inputs(args):
-    check = lambda fname: fname.lower().endswith(('.png', '.txt'))
-    for input_file in args.inputs:
-        if not check(input_file.name):
-            sys.stderr.write('All inputs must be .png or .txt')
-            sys.exit(1)
-        else:
+def process_args(args):
+    if args.idir is not None:
+        for path, dirs, files in os.walk(args.idir):
+            for filename in files:
+                fullname = os.path.join(path, filename)
+                process_input(fullname, args)
+                # result_fullname = os.path.join(args.dest, result_filename)
+
+    elif len(args.inputs) != 0:
+        for input_file in args.inputs:
             process_input(input_file, args)
+    else:
+        for path, dirs, files in os.walk(DEFAULT_INPUT_DIR):
+            for filename in files:
+                fullname = os.path.join(path, filename)
+                process_input(fullname, args)
+
 
 
 if __name__ == "__main__":
@@ -86,18 +97,23 @@ if __name__ == "__main__":
     parser.add_argument('-t',
                         action='store_true',
                         help='runs all tests')
-    parser.add_argument('-i',
+    parser.add_argument('-inv',
                         action='store_true',
                         help='inverts the picture colors')
+    parser.add_argument('-idir',
+                        help='the directory with inputs',
+                        default=DEFAULT_INPUT_DIR)
     parser.add_argument('inputs',
                         nargs='*',
                         type=argparse.FileType('r'),
                         help='A list of inputs (.PNG, .TXT)')
+
     args = parser.parse_args()
 
     if args.t is True:  # Runs tests
         print "Running tests..."
-        test_img2stl_helloworld()
-        test_txt2stl_braille()
+        test_txt2braille()
+        test_braille2img()
+        test_img2stl()
     else:
-        validate_inputs(args)
+        process_args(args)
