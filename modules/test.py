0ab99e5b31bcae1c9186a46c6087407d6a1f918d
Alexandr Timchenko <atimchenko92@gmail.com>
2019-08-01 14:41:25 +0200

    Added tests for Goethe and Dickens

--- modules/test.py
+++ modules/test.py
@@ -14,8 +14,7 @@ def test_img2stl():
         fname_png,
         fname_stl,
         smoothing=1,
-        scale=0.15,
-    )
+        scale=0.15)
 
 
 def test_braille2stl():
@@ -30,8 +29,7 @@ def test_braille2stl():
         smoothing=2,
         red_factor=4,
         scale=0.012,
-        min_thickness_percent=0.2
-    )
+        min_thickness_percent=0.2)
 
 
 def test_txt2braille():
@@ -40,7 +38,51 @@ def test_txt2braille():
     print(braille)
 
 
+def test_txt2braille_big_en():
+    txt = EXAMPLES_DIR + 'dickens.txt'
+    braille = text2braille(txt, 'en-GB-g2.ctb')
+    print('Dickens:\n"%s"' % braille)
+    return braille
+
+
+def test_txt2braille_big_de():
+    txt = EXAMPLES_DIR + 'goethe.txt'
+    braille = text2braille(txt, DEFAULT_TABLE)
+    print('Goethe:\n"%s"' % braille)
+    return braille
+
+
+def test_braille2stl_big_en(braille):
+    print('Processing braille: "%s"' % 'Dickens')
+    fname_png = DEFAULT_INPUT_DIR + 'test_braille2stl_big_en.png'
+    unicode2png(braille, fname_png)
+    fname_stl = get_new_fname(fname_png, OUTPUT_DIR, 'stl')
+    png2stl(
+        fname_png,
+        fname_stl,
+        smoothing=2,
+        red_factor=4,
+        scale=0.012,
+        min_thickness_percent=0.2)
+
+
+def test_braille2stl_big_de(braille):
+    print('Processing braille: "%s"' % 'Goethe')
+    fname_png = DEFAULT_INPUT_DIR + 'test_braille2stl_big_de.png'
+    unicode2png(braille, fname_png)
+    fname_stl = get_new_fname(fname_png, OUTPUT_DIR, 'stl')
+    png2stl(
+        fname_png,
+        fname_stl,
+        smoothing=2,
+        red_factor=4,
+        scale=0.012,
+        min_thickness_percent=0.2)
+
+
 def run_tests():
     test_img2stl()
     test_braille2stl()
     test_txt2braille()
+    test_braille2stl_big_en(test_txt2braille_big_en())
+    test_braille2stl_big_de(test_txt2braille_big_de())
