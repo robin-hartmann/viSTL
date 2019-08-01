97d76bb16d507d1fa773c3c4bf0189cfa89e10f8
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-08-01 11:16:58 +0200

    Add text2braille and refactoring
    - use parameter '-t' for liblouis table
    - test parameter is now called '-test'

--- modules/png2stl.py
+++ modules/png2stl.py
@@ -1,5 +1,4 @@
 from numpy import asarray
-from os import path
 from PIL import Image, ImageOps
 from pylab import imread
 from scipy.ndimage import gaussian_filter
@@ -10,18 +9,17 @@ def invert_image(fname):
     return asarray(ImageOps.invert(Image.open(fname)))
 
 
-def png2stl(fname, output_dir, should_invert=False, smoothing=0, red_factor=1, scale=0.1, min_thickness_percent=0.1):
+def png2stl(fname_png, fname_stl, should_invert=False, smoothing=0, red_factor=1, scale=0.1, min_thickness_percent=0.1):
     if should_invert:
-        data = invert_image(fname)
+        data = invert_image(fname_png)
     else:
-        data = 256 * imread(fname)
+        data = 256 * imread(fname_png)
 
     data = data[:, :, 2] + red_factor * data[:, :, 0]  # Compose RGBA channels to give depth -> B + red_factor * R
     data = gaussian_filter(data, smoothing)
-    out_fname = path.splitext(path.basename(fname))[0] + ".stl"
     numpy2stl(
         data,
-        output_dir + out_fname,
+        fname_stl,
         scale=scale,
         max_width=200,
         max_height=200,
--- modules/test.py
+++ modules/test.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from const import EXAMPLES_DIR, OUTPUT_DIR, DEFAULT_INPUT_DIR
+from const import EXAMPLES_DIR, OUTPUT_DIR, DEFAULT_INPUT_DIR, DEFAULT_TABLE
+from text2braille import text2braille
 from unicode2png import unicode2png
 from png2stl import png2stl
 
 
 def test_img2stl():
-    fname = EXAMPLES_DIR + "hello_world.png"
-    print("Processing test image: %s" % fname)
+    fname = EXAMPLES_DIR + 'hello_world.png'
+    print('Processing test image: %s' % fname)
     png2stl(
         fname,
         OUTPUT_DIR,
@@ -15,7 +16,6 @@ def test_img2stl():
     )
 
 
-# @todo
 def test_braille2stl():
     braille = u"⠠⠓⠑⠇⠇⠕⠀⠠⠸⠺⠖"
     print("Processing braille: '%s'" % "Hello World!")
@@ -30,12 +30,13 @@ def test_braille2stl():
     )
 
 
-# @todo
 def test_txt2braille():
-    raise NotImplementedError
+    txt = EXAMPLES_DIR + 'hello_world.txt'
+    braille = text2braille(txt, DEFAULT_TABLE)
+    print(braille)
 
 
 def run_tests():
     test_img2stl()
     test_braille2stl()
-    # test_txt2braille() @todo
+    test_txt2braille()
