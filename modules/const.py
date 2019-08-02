1ef3b1802d6f6751860881dc405aa145981da303
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-08-02 09:45:42 +0200

    Rename workspace subdirectories

--- modules/const.py
+++ modules/const.py
@@ -1,7 +1,7 @@
 EXAMPLES_DIR = 'examples/'
 WORKSPACE_DIR = 'workspace/'
-OUTPUT_DIR = WORKSPACE_DIR + 'out_stl/'
-DEFAULT_INPUT_DIR = WORKSPACE_DIR + 'in_png_txt/'
+OUTPUT_DIR = WORKSPACE_DIR + 'out/'
+DEFAULT_INPUT_DIR = WORKSPACE_DIR + 'in/'
 
 LIBLOUIS_PATH = 'third/liblouis/'
 TABLES_PATH = LIBLOUIS_PATH + 'tables/'
