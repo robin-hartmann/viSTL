4c1427e8b6f2d242c3ac5d1a232466ccd2dacba3
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-08-02 09:30:43 +0200

    Move processing into separate module

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+from sys import argv
 from modules.cli import parse_args, run as run_cli
 
 if __name__ == '__main__':
-    parsed_args = parse_args()
+    parsed_args = parse_args(argv[1:])
 
     # @todo
     # if parsed_args is not None:
--- modules/cli.py
+++ modules/cli.py
@@ -1,68 +1,11 @@
 import argparse
-import os
 
-from const import OUTPUT_DIR, DEFAULT_INPUT_DIR, DEFAULT_TABLE
-from text2braille import text2braille
-from unicode2png import unicode2png
-from png2stl import png2stl
+from const import DEFAULT_INPUT_DIR, DEFAULT_TABLE
 from test import run_tests
-from util import get_new_fname
+from processor import process_inputs
 
 
-def process_text(fname_txt, parsed_args):
-    print('Translating "%s" to braille...' % fname_txt)
-    braille = text2braille(fname_txt, parsed_args.t)
-    fname_png = get_new_fname(fname_txt, DEFAULT_INPUT_DIR, 'png')
-    print('Rendering braille to png at "%s"' % fname_png)
-    unicode2png(braille, fname_png)
-    fname_stl = get_new_fname(fname_png, OUTPUT_DIR, 'stl')
-    print('Converting png at "%s" to stl at "%s"' % (fname_png, fname_stl))
-    png2stl(
-        fname_png,
-        fname_stl,
-        should_invert=parsed_args.inv,
-        smoothing=2,
-        red_factor=4,
-        scale=0.09
-    )
-
-
-def process_image(fname_png, parsed_args):
-    fname_stl = get_new_fname(fname_png, OUTPUT_DIR, 'stl')
-    print('Converting png at "%s" to stl at "%s"' % (fname_png, fname_stl))
-    png2stl(
-        fname_png,
-        fname_stl,
-        should_invert=parsed_args.inv,
-        smoothing=2,
-        red_factor=0.5,
-        scale=0.08,
-        min_thickness_percent=0.2
-    )
-
-
-def process_input(fname, parsed_args):
-    print('Processing input: "%s"' % fname)
-    if fname.lower().endswith('.png'):
-        process_image(fname, parsed_args)
-    elif fname.lower().endswith('.txt'):
-        process_text(fname, parsed_args)
-    else:
-        print('All inputs must be .png or .txt, skipping...')
-
-
-def process_inputs(parsed_args):
-    if len(parsed_args.inputs) != 0:
-        for input_file in parsed_args.inputs:
-            process_input(input_file.name, parsed_args)
-    else:
-        for path, dirs, files in os.walk(DEFAULT_INPUT_DIR):
-            for filename in files:
-                fullname = os.path.join(path, filename)
-                process_input(fullname, parsed_args)
-
-
-def parse_args():
+def parse_args(args):
     parser = argparse.ArgumentParser(description='Image/Braille text'
                                      ' converter to .STL')
     parser.add_argument('-t',
@@ -84,7 +27,7 @@ def parse_args():
                         type=argparse.FileType('r'),
                         help='A list of inputs (.PNG, .TXT)')
 
-    return parser.parse_args()
+    return parser.parse_args(args)
 
 
 def run(parsed_args):
